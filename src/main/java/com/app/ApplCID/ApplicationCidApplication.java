5e99fb7d6bc2f5164189b6822ced962428cac601
Erick Maria <erickmaria08@gmail.com>
2018-10-08 01:19:37 -0300

    initial commit

---
+++ src/main/java/com/app/ApplCID/ApplicationCidApplication.java
@@ -0,0 +1,12 @@
+package com.app.ApplCID;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApplicationCidApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApplicationCidApplication.class, args);
+	}
+}
---
+++ src/test/java/com/app/ApplCID/ApplicationCidApplicationTests.java
@@ -0,0 +1,16 @@
+package com.app.ApplCID;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ApplicationCidApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
