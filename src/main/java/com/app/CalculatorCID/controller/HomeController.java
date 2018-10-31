2cf5dc715baa4c7f6ba3d90b63c44f042eaf1e99
Victor Alessander <alessander@protonmail.com>
2018-10-30 22:37:56 -0300

    Pequena alteração

--- src/main/java/com/app/CalculatorCID/controller/HomeController.java
+++ src/main/java/com/app/CalculatorCID/controller/HomeController.java
@@ -1,12 +1,14 @@
 package com.app.CalculatorCID.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
+@RequestMapping
 public class HomeController {
-    @RequestMapping(value = { "" , "/", "/home", "index", "inicio" } )
+    @GetMapping(value = { "" , "/", "/home", "index", "inicio" })
 	public ModelAndView home() {
 		return new ModelAndView("home");
 	}
